380271434badfde26f4b895ead01fe0918784bb5
nirarin7 <31749198+nirarin7@users.noreply.github.com>
2022-07-24 10:24:16 -0600

    Update PythonExcelSearch.py

--- PythonExcelSearch.py
+++ PythonExcelSearch.py
@@ -1,23 +1,15 @@
-
-
 import pandas as pd
 import numpy as np
 
 
 def mergeData(MainDataEntry, SearchDataEntry):
-
   main_workbook = MainDataEntry
   search_workbook = SearchDataEntry
   # results_workbook = OutputDataEntry
 
-
   df_main = pd.read_excel(main_workbook)
   df_search = pd.read_excel(search_workbook)
-
-
-
   df_results = pd.merge(df_search, df_main[['Info','SCN', 'Content']], on = 'SCN', how = 'left')
   
-
-  ## user decide the file name
+  ## user decide file name
   df_results.to_excel("OutputDataEntry.xlsx", index = False)
